df97103a96cd0c080b8d093fe60ec6e275505128
Randolf Scholz <rscholz@ismll.de>
2021-09-17 21:14:35 +0200

    added q_quantile loss + Electricity Tasks docstring

---
+++ tsdm/tasks/Template.py
@@ -0,0 +1,12 @@
+r"""Module Summary Line.
+
+Module description
+"""  # pylint: disable=line-too-long # noqa
+
+from __future__ import annotations
+
+import logging
+from typing import Final
+
+logger = logging.getLogger(__name__)
+__all__: Final[list[str]] = []
