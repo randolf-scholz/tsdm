7f03184e7c26d55fa5b76758e813505228b3a260
Randolf Scholz <rscholz@ismll.de>
2021-09-17 14:05:28 +0200

    v 0.1.2: tasks added

--- tsdm/tasks/__init__.py
+++ tsdm/tasks/__init__.py
@@ -1,7 +1,14 @@
-r"""Module Summary Line.
+r"""Tasks make running experiments easy & reproducible.
 
-Module description
+Task = Dataset + Evaluation Protocol
+
+For simplicity, the evaluation protocol is, in this iteration, restricted to a test metric,
+and a test_loader object.
+
+We decided to use a dataloader instead of, say, a split to cater to the question of
+forecasting horizons.
 """
+
 
 from __future__ import annotations
 
