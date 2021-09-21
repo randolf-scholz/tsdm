df0fea9dfd20c371d9798353383b35fe05862833
Randolf Scholz <rscholz@ismll.de>
2021-09-21 10:54:26 +0200

    added multi_norm

--- tsdm/util/__init__.py
+++ tsdm/util/__init__.py
@@ -3,7 +3,7 @@ import logging
 from typing import Final
 
 from tsdm.util import dataloaders, decorators, dtypes, regularity_tests, system, samplers
-from tsdm.util._norms import grad_norm, relative_error, scaled_norm
+from tsdm.util._norms import grad_norm, multi_norm, relative_error, scaled_norm
 from tsdm.util._utilfuncs import ACTIVATIONS, deep_dict_update, deep_kval_update
 
 
@@ -16,6 +16,7 @@ __all__: Final[list[str]] = [
     "relative_error",
     "scaled_norm",
     "grad_norm",
+    "mulit_norm",
 ] + [
     "dataloaders",
     "decorators",
