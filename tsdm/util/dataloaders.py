7558e61e9f8b9f949991f74662bb7fe9bc5a160e
Randolf Scholz <rscholz@ismll.de>
2021-09-21 11:37:13 +0200

    moved samplers to own subrepo

--- tsdm/util/dataloaders.py
+++ tsdm/util/dataloaders.py
@@ -10,11 +10,9 @@ We implement multiple levels of abstraction.
 """
 
 import logging
-from typing import Callable, Final, Iterator, Optional, Sequence, Sized, TypeVar, Union
+from typing import Final
 
-import numpy as np
 import torch
-from numpy.typing import NDArray
 from torch import Tensor
 from torch.nn.utils.rnn import (
     PackedSequence,
@@ -23,7 +21,6 @@ from torch.nn.utils.rnn import (
     pad_packed_sequence,
     pad_sequence,
 )
-from torch.utils.data import Sampler
 
 LOGGER = logging.getLogger(__name__)
 __all__: Final[list[str]] = [
@@ -32,13 +29,7 @@ __all__: Final[list[str]] = [
     "collate_padded",
     "unpad_sequence",
     "upack_sequence",
-    "SliceSampler",
-    "TimeSliceSampler",
 ]
-
-
-T = TypeVar("T")
-Sample = Union[T, Callable[[], T]]
 
 
 def collate_list(batch: list[Tensor]) -> list[Tensor]:
@@ -87,86 +78,4 @@ def unpad_sequence():
     print(help(pack_padded_sequence))
 
 
-class TimeSliceSampler(Sampler):
-    """TODO: add class."""
-
-    def __init__(self, data_source: Optional[Sized]):
-        """TODO: Add method."""
-        super().__init__(data_source)
-
-    def __iter__(self) -> Iterator:
-        """TODO: Add method."""
-        return super().__iter__()
-
-
 # TODO: add exclusive_args decorator
-class SliceSampler(Sampler):
-    r"""Sample by index.
-
-    Default modus operandi:
-
-    - Use fixed window size
-    - Sample starting index uniformly from [0:-window]
-
-    Should you want to sample windows of varying size, you may supply a
-
-    Attributes
-    ----------
-    data:
-    idx: range(len(data))
-    rng: a numpy random Generator
-    """
-
-    data: Sequence
-    idx: NDArray
-    rng: np.random.Generator
-
-    def __init__(
-        self,
-        data_source: Sequence,
-        slice_sampler: Optional[Sample[int]] = None,
-        sampler: Optional[Callable[[], tuple[int, int]]] = None,
-    ):
-        super().__init__(data_source)
-        self.data = data_source
-        self.idx = np.arange(len(data_source))
-        self.rng = np.random.default_rng()
-
-        def slicesampler_dispatch() -> Callable[[], int]:
-            if slice_sampler is None:
-                return lambda: max(1, len(data_source) // 10)
-            if isinstance(slice_sampler, int):
-                return lambda: slice_sampler  # type: ignore
-            if callable(slice_sampler):
-                return slice_sampler
-            raise NotImplementedError("slice_sampler not compatible.")
-
-        self._slice_sampler = slicesampler_dispatch()
-
-        def default_sampler() -> tuple[int, int]:
-            window_size: int = self._slice_sampler()
-            start_index: int = self.rng.choice(self.idx[:-window_size])
-            return window_size, start_index
-
-        self._sampler = default_sampler if sampler is None else sampler
-
-    def slice_sampler(self) -> int:
-        r"""Return random window size."""
-        return self._slice_sampler()
-
-    def sampler(self) -> tuple[int, int]:
-        r"""Return random start_index and window_size."""
-        return self._sampler()
-
-    def __iter__(self) -> Iterator:
-        r"""Yield random slice from dataset.
-
-        Returns
-        -------
-        Iterator
-        """
-        while True:
-            # sample len and index
-            window_size, start_index = self.sampler()
-            # return slice
-            yield self.data[start_index : start_index + window_size]
