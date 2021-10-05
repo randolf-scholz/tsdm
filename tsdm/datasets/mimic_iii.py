20af11dd81561d23f5392b4b0890d822b3ea13ef
Randolf Scholz <rscholz@ismll.de>
2021-10-06 00:02:25 +0200

    fixed mimic-iii file name

--- tsdm/datasets/mimic_iii.py
+++ tsdm/datasets/mimic_iii.py
@@ -41,24 +41,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 class MIMIC_III(BaseDataset):
-    r"""MIMIC-III Clinical Database.
-
-    Abstract
-    --------
-
-    MIMIC-III is a large, freely-available database comprising deidentified health-related
-    data associated with over forty thousand patients who stayed in critical care units of
-    the Beth Israel Deaconess Medical Center between 2001 and 2012.
-    The database includes information such as demographics, vital sign measurements made at
-    the bedside (~1 data point per hour), laboratory test results, procedures, medications,
-    caregiver notes, imaging reports, and mortality (including post-hospital discharge).
-
-    MIMIC supports a diverse range of analytic studies spanning epidemiology, clinical
-    decision-rule improvement, and electronic tool development. It is notable for three
-    factors: it is freely available to researchers worldwide; it encompasses a diverse and
-    very large population of ICU patients; and it contains highly granular data, including
-    vital signs, laboratory results, and medications.
-    """
+    r"""MIMIC-III Clinical Database."""
 
     url: str = r"https://physionet.org/files/mimiciii/1.4/"
     info_url: str = r"https://physionet.org/content/mimiciii/1.4/"
