babcb7acb3ab9c9da9a3e3e774328242de942ad4
Randolf Scholz <rscholz@ismll.de>
2021-11-04 09:02:05 +0100

    updated config

--- tsdm/config/__init__.py
+++ tsdm/config/__init__.py
@@ -23,95 +23,28 @@ __all__ = [
     "RAWDATADIR",
     "DEFAULT_DEVICE",
     "DEFAULT_DTYPE",
+    "conf",
+    # Classes
+    "Config",
 ]
 
 import logging
-from importlib import resources
-from pathlib import Path
 
-import torch
-import yaml
-
-from tsdm.config import config_files
+from tsdm.config._config import (  # CONSTANTS; Classes
+    BASEDIR,
+    CONFIG,
+    DATASETDIR,
+    DATASETS,
+    DEFAULT_DEVICE,
+    DEFAULT_DTYPE,
+    HASHES,
+    HOMEDIR,
+    LOGDIR,
+    MODELDIR,
+    MODELS,
+    RAWDATADIR,
+    Config,
+    conf,
+)
 
 __logger__ = logging.getLogger(__name__)
-
-DEFAULT_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-DEFAULT_DTYPE = torch.float32
-
-with resources.path(config_files, "config.yaml") as file:
-    with open(file, "r", encoding="utf8") as fname:
-        CONFIG = yaml.safe_load(fname)
-        r"""Dictionary containing basic configuration of TSDM."""
-
-with resources.path(config_files, "models.yaml") as file:
-    with open(file, "r", encoding="utf8") as fname:
-        MODELS = yaml.safe_load(fname)
-        r"""Dictionary containing sources of the available models."""
-
-with resources.path(config_files, "datasets.yaml") as file:
-    with open(file, "r", encoding="utf8") as fname:
-        DATASETS = yaml.safe_load(fname)
-        r"""Dictionary containing sources of the available datasets."""
-
-with resources.path(config_files, "hashes.yaml") as file:
-    with open(file, "r", encoding="utf8") as fname:
-        HASHES = yaml.safe_load(fname)
-        r"""Dictionary containing hash values for both models and datasets."""
-
-HOMEDIR = Path.home()
-r"""The users home directory."""
-
-BASEDIR = HOMEDIR.joinpath(CONFIG["basedir"])
-r"""Root directory for tsdm storage."""
-
-LOGDIR = BASEDIR.joinpath(CONFIG["logdir"])
-r"""Path where logfiles are stored."""
-
-MODELDIR = BASEDIR.joinpath(CONFIG["modeldir"])
-r"""Path where imported models are stored."""
-
-DATASETDIR = BASEDIR.joinpath(CONFIG["datasetdir"])
-r"""Path where preprocessed datasets are stored."""
-
-RAWDATADIR = BASEDIR.joinpath(CONFIG["rawdatadir"])
-r"""Path where raw imported datasets are stored."""
-
-LOGDIR.mkdir(parents=True, exist_ok=True)
-# logging.basicConfig(
-#     filename=str(LOGDIR.joinpath("example.log")),
-#     filemode="w",
-#     format="[%(asctime)s] [%(levelname)-s]\t[%(name)s]\t%(message)s, (%(filename)s:%(lineno)s)",
-#     datefmt="%Y-%m-%d %H:%M:%S",
-#     level=logging.INFO)
-
-__logger__.info("Available Models: %s", set(MODELS))
-__logger__.info("Available Datasets: %s", set(DATASETS))
-
-
-def generate_folders(d: dict, current_path: Path):
-    """Create nested folder structure based on nested dictionary keys.
-
-    source: `StackOverflow <https://stackoverflow.com/a/22058144/9318372>`_
-
-    Parameters
-    ----------
-    current_path: Path
-    d: dict
-
-    Returns
-    -------
-    None
-    """
-    for directory in d:
-        path = current_path.joinpath(directory)
-        if d[directory] is None:
-            __logger__.info("creating folder %s", path)
-            path.mkdir(parents=True, exist_ok=True)
-        else:
-            generate_folders(d[directory], path)
-
-
-# __logger__.info(F"Found config files: {set(resources.contents('config_files'))}")
-__logger__.info("Initializing Folder Structure")
-generate_folders(CONFIG["folders"], BASEDIR)
