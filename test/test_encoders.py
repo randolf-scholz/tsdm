1be6f46731a2f1bcd96dffea3b0db193f07be9fe
Randolf Scholz <rscholz@ismll.de>
2021-11-09 02:30:12 +0100

    unittest for encoder

--- test/test_encoders.py
+++ test/test_encoders.py
@@ -2,42 +2,34 @@ r"""Test converters to masked format etc."""
 
 import logging
 
-from pandas import NA, DataFrame, testing
+from pandas import Series, date_range, testing
 
-from tsdm.encoders.functional import make_masked_format
+from tsdm.encoders.modular import DateTimeEncoder
 
 LOGGER = logging.getLogger(__name__)
 
 
-def test_make_masked_format():
-    r"""Using example taken from Figure 2 in [1].
+def test_datetime_encoder():
+    r"""Test whether the encoder is reversible."""
+    time = date_range("2020-01-01", "2021-01-01", freq="1d")
+    encoder = DateTimeEncoder()
+    encoder.fit(time)
+    encoded = encoder.encode(time)
+    decoded = encoder.decode(encoded)
+    testing.assert_series_equal(time, decoded)
 
-    References
-    ----------
-    1. Recurrent Neural Networks for Multivariate Time Series with Missing Values
-       Che et. al., Nature 2017
-    """
-    x = [[47, 49, NA, 40, NA, 43, 55], [NA, 15, 14, NA, NA, NA, 15]]
-    t = [0, 0.1, 0.6, 1.6, 2.2, 2.5, 3.1]
-
-    d = [[0.0, 0.1, 0.5, 1.5, 0.6, 0.9, 0.6], [0.0, 0.1, 0.5, 1.0, 1.6, 1.9, 2.5]]
-
-    m = [[1, 1, 0, 1, 0, 1, 1], [0, 1, 1, 0, 0, 0, 1]]
-
-    data = DataFrame(x, columns=t).T
-    mask = DataFrame(m, columns=t).T
-    diff = DataFrame(d, columns=t).T
-
-    x, m, d = make_masked_format(data)
-    testing.assert_frame_equal(x, data, check_dtype=False)
-    testing.assert_frame_equal(m, mask, check_dtype=False)
-    testing.assert_frame_equal(d, diff, check_dtype=False)
+    time = Series(time)
+    encoder = DateTimeEncoder()
+    encoder.fit(time)
+    encoded = encoder.encode(time)
+    decoded = encoder.decode(encoded)
+    testing.assert_series_equal(time, decoded)
 
 
 def __main__():
     logging.basicConfig(level=logging.INFO)
     LOGGER.info("Testing MASKED_FORMAT started!")
-    test_make_masked_format()
+    test_datetime_encoder()
     LOGGER.info("Testing MASKED_FORMAT finished!")
 
 
