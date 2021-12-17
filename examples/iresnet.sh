b31050612584de44c180c9c88e9a33ae9b937f43
Randolf Scholz <rscholz@ismll.de>
2021-12-17 12:26:06 +0100

    updated sbatch

--- examples/iresnet.sh
+++ examples/iresnet.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=KIWI
-#SBATCH --partition=GPU
+#SBATCH --partition=PGPU
 #SBATCH --output=logs/%j-%x.stdout.log
 #SBATCH --error=logs/%j-%x.stderr.log
 #SBATCH --gpus=1
 
 
 source activate kiwi
-
+mkdir -p logs
 srun python KIWI_RUNS_EXPERIMENT.py
