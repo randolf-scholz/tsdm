4b1a3cf2d0307d516f8298c82f5289855d9fa134
Randolf Scholz <rscholz@ismll.de>
2022-01-31 14:35:10 +0100

    generic torch datasets

---
+++ tsdm/datasets/torch/generic.py
@@ -0,0 +1,5 @@
+r"""Generic Dataset classes."""
+
+import logging
+
+__logger__ = logging.getLogger(__name__)
---
+++ tsdm/tasks/kiwi_final_product.py
@@ -0,0 +1,8 @@
+r"""#TODO add module summary line.
+
+#TODO add module description.
+"""
+
+import logging
+
+__logger__ = logging.getLogger(__name__)
