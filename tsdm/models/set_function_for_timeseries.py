3521ddfa6e9243dc7749ab93adfac2c9d5e2a244
Randolf Scholz <rscholz@ismll.de>
2022-02-02 10:45:18 +0100

    experiments running

--- tsdm/models/set_function_for_timeseries.py
+++ tsdm/models/set_function_for_timeseries.py
@@ -34,6 +34,19 @@ class SetFuncTS(nn.Module):
       | https://proceedings.mlr.press/v119/horn20a.html
     - https://github.com/BorgwardtLab/Set_Functions_for_Time_Series
     """
+
+    HP: dict = {
+        "__name__": __qualname__,  # type: ignore[name-defined]
+        "__doc__": __doc__,
+        "__module__": __module__,  # type: ignore[name-defined]
+        "time_encoder": PositionalEncoder.HP,
+        "key_encoder": DeepSet.HP,
+        "value_encoder": MLP.HP,
+        "attention": ScaledDotProductAttention.HP,
+        "head": MLP.HP,
+    }
+    r"""Dictionary of hyperparameters."""
+
     # BUFFER
     dummy: Tensor
 
@@ -50,12 +63,13 @@ class SetFuncTS(nn.Module):
 
         dim_keys = input_size if dim_keys is None else dim_keys
         dim_vals = input_size if dim_vals is None else dim_vals
-        dim_time = 10 if dim_time is None else dim_time
+        dim_time = 8 if dim_time is None else dim_time
         latent_size = input_size if latent_size is None else latent_size
         # time_encoder
         # feature_encoder -> CNN?
-        self.time_encoder = PositionalEncoder(dim_time, scale=1.0)
+        self.time_encoder = PositionalEncoder(dim_time, scale=10.0)
         self.key_encoder = DeepSet(input_size + dim_time - 1, dim_keys)
+        print(dim_vals)
         self.value_encoder = MLP(input_size + dim_time - 1, dim_vals)
         self.attn = ScaledDotProductAttention(
             dim_keys + input_size + dim_time - 1, dim_vals, latent_size
@@ -104,7 +118,12 @@ class SetFuncTS(nn.Module):
         return y
 
     @jit.export
-    def batch_forward(self, batch: list[tuple[Tensor, Tensor, Tensor]]) -> Tensor:
+    def forward_tuple(self, t: tuple[Tensor, Tensor, Tensor]) -> Tensor:
+        r"""Signature: `(∗V, T), (*V, D), (*V, K) → (..., F)."""
+        return self.forward(t[0], t[1], t[2])
+
+    @jit.export
+    def forward_batch(self, batch: list[tuple[Tensor, Tensor, Tensor]]) -> Tensor:
         r"""Signature: `[(∗V, T), (*V, D), (*V, K)] → (..., F)`.
 
         Parameters
