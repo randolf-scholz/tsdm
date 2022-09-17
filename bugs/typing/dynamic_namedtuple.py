5395d693830245ac3b2649386e7c1744c131a654
Randolf Scholz <randolf.scholz@gmail.com>
2022-09-17 12:53:38 +0200

    tuple stuff

--- bugs/typing/dynamic_namedtuple.py
+++ bugs/typing/dynamic_namedtuple.py
@@ -42,28 +42,74 @@ class _NamedTuple(tuple[T, ...], ABC):
         ...
 
 
-def register_namedtuple(fields: list[str], /, *, name: str) -> type[_NamedTuple]:
+def register_namedtuple(obj: object, fields: list[str], /, *, name: str) -> None:
     if not name.isidentifier():
         raise ValueError(f"{name} is not a valid identifier!")
-    tuple_type: type[tuple] = namedtuple(name, fields)  # type: ignore[misc]
-    reveal_type(tuple_type)
+    tuple_type = cast(type[_NamedTuple], namedtuple(name, fields))
+    # reveal_type(tuple_type)
     _NamedTuple.register(tuple_type)
-    return cast(type[_NamedTuple], tuple_type)
+
+    # create a unique identifier and store it in globals
+    identifier = f"_{name}_{obj.__class__.__name__}_{hash(obj)}"
+    tuple_type.__qualname__ = identifier
+    if identifier in globals():
+        raise RuntimeError(f"A class of name '{identifier}' exists!!")
+    globals()[identifier] = tuple_type
+
+    setattr(obj, "tuple_type", tuple_type)
+
+    # if hasattr(obj, "__del__"):
+
+    # return tuple_type
 
 
 class Foo:
-    tuple: type[_NamedTuple]
+    tuple_type: type[_NamedTuple]
 
     def __init__(self, fields: list[str]):
         super().__init__()
-        self.tuple = register_namedtuple(fields, name="FooTuple")
+        register_namedtuple(self, fields, name="FooTuple")
+
+        # self.tuple_type = register_namedtuple(self, fields, name="FooTuple")
 
 
 foo = Foo(["a", "b", "c"])
-FooTup = foo.tuple
-footup = foo.tuple(1, 2, 3)
+FooTup = foo.tuple_type
+footup = foo.tuple_type(1, 2, 3)
 
 assert isinstance(footup, tuple)
 assert issubclass(FooTup, tuple)
 assert isinstance(footup, _NamedTuple)
 assert issubclass(FooTup, _NamedTuple)
+
+from collections import namedtuple
+
+
+class ClassWithTuple:
+    tuple_id: str
+    tuple_type: type[tuple]
+
+    def __init__(self, name: str, fields: list[str]) -> None:
+        tuple_type = namedtuple(name, fields)
+
+        # create a unique identifier and store it in globals
+        tuple_id = f"_{name}_{self.__class__.__name__}_{hash(self)}"
+        tuple_type.__qualname__ = tuple_id
+
+        if tuple_id in globals():
+            raise RuntimeError(f"A class '{identifier}' exists!")
+        globals()[tuple_id] = tuple_type
+
+        self.tuple_id = tuple_id
+        self.tuple_type = tuple_type
+
+    def __call__(self, *args) -> tuple:
+        return self.tuple_type(*args)
+
+    def __del__(self):
+        del globals()[self.tuple_id]
+        del self
+
+
+obj = ClassWithTuple("FooTuple", ["a", "b", "c"])
+obj(1, 2, 3)
