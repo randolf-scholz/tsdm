2aa5abd98a9ec2bb235c17f894013d8a4d220ca4
Randolf Scholz <randolf.scholz@gmail.com>
2022-10-17 19:09:45 +0200

    fixed data_overview'

--- src/tsdm/random/stats/_stats.py
+++ src/tsdm/random/stats/_stats.py
@@ -33,26 +33,39 @@ def sparsity(df: DataFrame) -> tuple[float, float]:
 #     r"""Summary statistics: column-wise mean/median/std/histogram. Cross-channel correlation."""
 
 
-def data_overview(df: DataFrame, index_col: Optional[Hashable] = None) -> DataFrame:
+def data_overview(
+    df: DataFrame, index_col: Optional[Hashable] = None, digits: int = 2
+) -> DataFrame:
     r"""Get a summary of the data."""
     overview = DataFrame(index=df.columns)
     mask = df.isna()
     numerical_cols = df.select_dtypes(include="number").columns
-    float_cols = df.select_dtypes(include="float").columns
+    # float_cols = df.select_dtypes(include="float").columns
     # other_cols = df.select_dtypes(exclude="number").columns
 
-    overview["# datapoints"] = (~mask).sum()
-    overview["% missing"] = (mask.mean() * 100).round(2)
+    overview["datapoints"] = (~mask).sum()
+    overview["missing"] = (mask.mean() * 100).round(2)
 
-    overview.loc[numerical_cols, "min"] = df[numerical_cols].min().round(2)
-    overview.loc[numerical_cols, "mean"] = df[numerical_cols].mean().round(2)
-    overview.loc[numerical_cols, "std"] = df[numerical_cols].std().round(2)
-    overview.loc[numerical_cols, "max"] = df[numerical_cols].max().round(2)
+    overview.loc[numerical_cols, "min"] = df[numerical_cols].min()
+    overview.loc[numerical_cols, "mean"] = df[numerical_cols].mean()
+    overview.loc[numerical_cols, "std"] = df[numerical_cols].std()
+    overview.loc[numerical_cols, "max"] = df[numerical_cols].max()
 
-    overview.loc[float_cols, "min"] = overview.loc[float_cols, "min"].round(2)
-    overview.loc[float_cols, "mean"] = overview.loc[float_cols, "mean"].round(2)
-    overview.loc[float_cols, "std"] = overview.loc[float_cols, "std"].round(2)
-    overview.loc[float_cols, "max"] = overview.loc[float_cols, "max"].round(2)
+    # fmt: off
+    column_dtypes = {
+        "datapoints" : "Int64",
+        "missing"    : "Float64",
+        "min"        : "Float64",
+        "mean"       : "Float64",
+        "std"        : "Float64",
+        "max"        : "Float64",
+    }
+    # fmt: on
+
+    overview = overview.astype(column_dtypes)
+    for col, dtype in column_dtypes.items():
+        if pandas.api.types.is_float_dtype(dtype):
+            overview[col] = overview[col].round(digits)
 
     if index_col is not None:
         freq = {}
