6d3b6b27225189ea92bd667b266e9cea2e096dfc
Randolf Scholz <randolf.scholz@gmail.com>
2022-10-20 00:38:29 +0200

    refactored old kiwi runs

--- bugs/pandas-stubs/dataframe_init.py
+++ bugs/pandas-stubs/dataframe_init.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
-from collections.abc import Sequence
+from collections.abc import MutableSequence, Sequence
 
 import pandas as pd
 
-index = [3, 2, 7]
+index: tuple[int, int, int] = [3, 2, 7]
 columns = ["a", "b", "c"]
+
+# index = "abc"
+
 df = pd.DataFrame(index=index, columns=columns)  # ✘ error: [call-overload]
 print(df)
