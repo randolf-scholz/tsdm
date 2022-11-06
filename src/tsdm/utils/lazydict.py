a9b0a03658a5e7c60fb0974aa850c60651c42119
Randolf Scholz <randolf.scholz@gmail.com>
2022-11-06 01:34:11 +0100

    fixed LazyDict copy

--- src/tsdm/utils/lazydict.py
+++ src/tsdm/utils/lazydict.py
@@ -4,6 +4,7 @@ The LazyDict is a dictionary that is initialized with functions as the values.
 Once the value is accessed, the function is called and the result is stored.
 """
 
+from __future__ import annotations
 
 __all__ = [
     # Classes
@@ -12,7 +13,7 @@ __all__ = [
 ]
 
 import logging
-from collections.abc import Callable, Iterable, Iterator, Mapping, MutableMapping
+from collections.abc import Callable, Iterable, Mapping, MutableMapping
 from typing import Any, NamedTuple, TypeAlias, Union, overload
 
 from tsdm.utils._util import get_function_args, is_positional
@@ -39,9 +40,12 @@ class LazyFunction(NamedTuple):
 
 
 FuncSpec: TypeAlias = Union[
+    LazyFunction,
     Callable[[], ObjectVar],
     Callable[[KeyVar], ObjectVar],
-    tuple[Callable[..., ObjectVar]],  # no args
+    tuple[LazyFunction],
+    tuple[Callable[[], ObjectVar]],  # no args
+    tuple[Callable[[KeyVar], ObjectVar]],  # key arg
     tuple[Callable[..., ObjectVar], tuple],  # args
     tuple[Callable[..., ObjectVar], dict],  # kwargs
     tuple[Callable[..., ObjectVar], tuple, dict],  # args, kwargs
@@ -76,25 +80,31 @@ class LazyDict(dict[KeyVar, ObjectVar]):
         cls.popitem = MutableMapping.popitem  # type: ignore[assignment]
         cls.setdefault = MutableMapping.setdefault  # type: ignore[assignment]
         cls.update = MutableMapping.update  # type: ignore[assignment]
-        return super().__new__(cls)
+        return super().__new__(cls, *args, **kwargs)
 
     @overload
-    def __init__(self, /, **kwargs: ObjectVar) -> None:
+    def __init__(self, /, **kwargs: FuncSpec | ObjectVar) -> None:
         ...
 
     @overload
     def __init__(
-        self, mapping: Mapping[KeyVar, FuncSpec], /, **kwargs: ObjectVar
+        self,
+        mapping: Mapping[KeyVar, FuncSpec | ObjectVar],
+        /,
+        **kwargs: FuncSpec | ObjectVar,
     ) -> None:
         ...
 
     @overload
     def __init__(
-        self, iterable: Iterable[tuple[KeyVar, FuncSpec]], /, **kwargs: ObjectVar
+        self,
+        iterable: Iterable[tuple[KeyVar, FuncSpec | ObjectVar]],
+        /,
+        **kwargs: FuncSpec | ObjectVar,
     ) -> None:
         ...
 
-    def __init__(self, /, *args: Any, **kwargs: ObjectVar) -> None:
+    def __init__(self, /, *args, **kwargs):
         r"""Initialize the dictionary."""
         super().__init__()
         MutableMapping.update(self, *args, **kwargs)
@@ -146,117 +156,6 @@ class LazyDict(dict[KeyVar, ObjectVar]):
         r"""Return the representation of the dictionary."""
         return repr_mapping(self, title=self.__class__.__name__, repr_fun=repr)
 
-
-class LazyDict2(MutableMapping[KeyVar, ObjectVar]):
-    r"""A Lazy Dictionary implementation.
-
-    Values are allowed to be one of the following:
-
-    - LazyFunction
-    - Callable that takes exactly 0 mandatory args
-    - Callable that takes axactly 1 mandatory positional arg and no mandatory kwargs
-      - In this case, the key will be used as the first argument
-    - tuple of the form tuple[Callable] as above
-    - tuple of the form tuple[Callable, tuple]
-    - tuple of the form tuple[Callable, dict]
-    - tuple of the form tuple[Callable, tuple, dict]
-    """
-
-    @overload
-    def __init__(self, /, **kwargs: ObjectVar) -> None:
-        ...
-
-    @overload
-    def __init__(
-        self, mapping: Mapping[KeyVar, FuncSpec], /, **kwargs: ObjectVar
-    ) -> None:
-        ...
-
-    @overload
-    def __init__(
-        self, iterable: Iterable[tuple[KeyVar, FuncSpec]], /, **kwargs: ObjectVar
-    ) -> None:
-        ...
-
-    def __init__(self, /, *args: Any, **kwargs: ObjectVar) -> None:
-        r"""Initialize the dictionary.
-
-        Tuples of the form (key, (Callable[..., Any], args, kwargs))
-        Dict of the form {key: (Callable[..., Any], args, kwargs)}
-        """
-        self._dict: dict[Any, Any] = {}
-        # self._initialized: dict[Any, bool] = {}
-
-        if len(args) > 1:
-            raise TypeError("Too many positional arguments")
-        else:
-            self.update(*args)
-
-        self.update(**kwargs)
-
-    @staticmethod
-    def _make_lazy_function(
-        key: KeyVar,
-        value: FuncSpec | ObjectVar,
-    ) -> LazyFunction:
-        match value:
-            case LazyFunction():
-                return value  # type: ignore[return-value]
-            case Callable():  # type: ignore[misc]
-                args = get_function_args(value, mandatory=True)  # type: ignore[unreachable]
-                match nargs := len(args):
-                    case 0:
-                        return LazyFunction(func=value)
-                    case 1 if all(
-                        is_positional(p) for p in args
-                    ):  # set the key as input
-                        return LazyFunction(func=value, args=(key,))
-                    case _:
-                        raise TypeError(f"Function {value} requires {nargs} arguments.")
-            case [Callable()]:  # type: ignore[misc]
-                return LazyDict._make_lazy_function(key, value[0])  # type: ignore[unreachable]
-            case Callable(), tuple():  # type: ignore[misc]
-                return LazyFunction(func=value[0], args=value[1])  # type: ignore[unreachable]
-            case Callable(), dict():  # type: ignore[misc]
-                return LazyFunction(func=value[0], kwargs=value[1])  # type: ignore[unreachable]
-            case Callable(), tuple(), dict():  # type: ignore[misc]
-                return LazyFunction(value[0], args=value[1], kwargs=value[2])  # type: ignore[unreachable]
-            case _:
-                return LazyFunction(lambda: value)
-
-    def __getitem__(self, key: KeyVar) -> ObjectVar:
-        r"""Get the value of the key."""
-        value = self._dict[key]
-        if isinstance(value, LazyFunction):
-            new_value = value()
-            self._dict[key] = new_value
-            return new_value
-        return value
-
-    def __setitem__(self, key: KeyVar, value: FuncSpec | ObjectVar) -> None:
-        r"""Set the value of the key."""
-        self._dict[key] = self._make_lazy_function(key, value)
-
-    def __delitem__(self, key: KeyVar) -> None:
-        r"""Delete the value of the key."""
-        del self._dict[key]
-
-    def __iter__(self) -> Iterator[KeyVar]:
-        r"""Iterate over the keys."""
-        return iter(self._dict)
-
-    def __len__(self) -> int:
-        r"""Return the number of keys."""
-        return len(self._dict)
-
-    def __repr__(self) -> str:
-        r"""Return the representation of the dictionary."""
-        return repr_mapping(self._dict, title=self.__class__.__name__, repr_fun=repr)
-
-    def __str__(self):
-        r"""Return the string representation of the dictionary."""
-        return str(self._dict)
-
-
-# x : Callable[[int, ...], float]
-# reveal_type(x)
+    def copy(self) -> LazyDict[KeyVar, ObjectVar]:
+        r"""Return a shallow copy of the dictionary."""
+        return self.__class__(self)
---
+++ tests/utils/test_lazydict.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+r"""Test LazyDict."""
+
+import logging
+from collections.abc import MutableMapping
+
+from tsdm.utils.lazydict import LazyDict, LazyFunction
+
+__logger__ = logging.getLogger(__name__)
+
+
+def test_lazydict():
+    r"""Test LazyDict."""
+
+    def no_input():
+        return 0
+
+    def single_input(x):
+        return x
+
+    def positional_only(a, /, b, c=1, *args):
+        return a + b + c + sum(args)
+
+    def keyword_only(*, d, e=2, **kwargs):
+        return d + e + sum(kwargs.values())
+
+    def generic(a, /, b, c=1, *args, d, e=2, **kwargs):
+        return a + b + c + sum(args) + d + e + sum(kwargs.values())
+
+    d = {
+        0: no_input,
+        1: single_input,
+        2: (no_input,),
+        3: (single_input,),
+        4: (positional_only, (1, 1, 1, 1)),
+        5: (keyword_only, {"d": 1, "e": 1, "f": 1, "g": 1}),
+        6: (generic, (1, 1, 1, 1), {"d": 1, "e": 1, "f": 1, "g": 1}),
+    }
+    ld = LazyDict(d, answer=42)
+
+    assert isinstance(ld, LazyDict)
+    assert isinstance(ld, dict)
+    assert isinstance(ld, MutableMapping)
+
+    for value in ld.values():
+        assert isinstance(value, LazyFunction)
+
+    for key in ld:
+        assert isinstance(ld[key], int)
+
+
+def test_fromkeys():
+    r"""Test if fromkeys works."""
+    ld = LazyDict.fromkeys([1, 2, 3], 0)
+
+    assert isinstance(ld, LazyDict)
+    assert isinstance(ld, dict)
+    assert isinstance(ld, MutableMapping)
+
+    for value in ld.values():
+        assert isinstance(value, LazyFunction)
+
+    for key in ld:
+        assert isinstance(ld[key], int)
+
+
+def test_copy():
+    r"""Test if copying works."""
+    ld = LazyDict.fromkeys([1, 2, 3], 0)
+    ld2 = ld.copy()
+
+    assert isinstance(ld2, LazyDict)
+    assert isinstance(ld, dict)
+    assert isinstance(ld, MutableMapping)
+
+    for value in ld2.values():
+        assert isinstance(value, LazyFunction)
+
+    for key in ld2:
+        assert isinstance(ld2[key], int)
+
+
+def __main__() -> None:
+    logging.basicConfig(level=logging.INFO)
+    __logger__.info("Testing lazydict ...")
+    test_lazydict()
+    __logger__.info("Done.")
+
+
+if __name__ == "__main__":
+    __main__()
