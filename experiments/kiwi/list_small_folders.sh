c68370bf0ef0a65bdaa845b7c8cf212243958c68
Randolf Scholz <randolf.scholz@gmail.com>
2022-11-17 10:11:21 +0100

    minor edits

--- experiments/kiwi/list_small_folders.sh
+++ experiments/kiwi/list_small_folders.sh
@@ -1,8 +1,12 @@
 #!/bin/env bash
 
+# recursively list all directories if their size is less than 10KB
+# find . -type d -size -10k -exec du -sh {} \;
+
+
 # recursively list all directories and their size with less than 10KB
 find . -type d -print0 | while read -d $'\0' dir; do
-    if [ $(du -s "$dir" | cut -f1) -lt 10 ]; then
+    if [ $(du -s "$dir" | cut -f1) -lt 16 ]; then
         du -sh "$dir"
     fi
 done
@@ -13,7 +17,7 @@ read -p "Delete the above directories? [y/N] " -n 1 -r
 # if yes, delete the directories
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     find . -type d -print0 | while read -d $'\0' dir; do
-        if [ $(du -s "$dir" | cut -f1) -lt 10 ]; then
+        if [ $(du -s "$dir" | cut -f1) -lt 16 ]; then
             rm -rf "$dir"
             echo "Deleted $dir"
         fi
