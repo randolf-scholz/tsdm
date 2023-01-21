add8a18e94b1376dbf46c814aa91f7c2f28d6da5
Randolf Scholz <randolf.scholz@gmail.com>
2023-01-22 00:52:15 +0100

    updated utils

--- src/tsdm/utils/remote.py
+++ src/tsdm/utils/remote.py
@@ -1,15 +1,17 @@
 r"""Implements a downloader for the TSDM-package."""
 
 __all__ = [
-    "hash_file",
     "download",
+    "hash_file",
+    "import_from_url",
     "validate_hash",
 ]
 
 import hashlib
-import string
+import subprocess
 from pathlib import Path
 from typing import Any, Optional
+from urllib.parse import urlparse
 
 import requests
 from tqdm.autonotebook import tqdm
@@ -95,27 +97,28 @@ def download(
         validate_hash(path, hash_value, hash_algorithm=hash_algorithm, **hash_kwargs)
 
 
-def to_base(n: int, b: int) -> list[int]:
-    r"""Convert non-negative integer to any basis.
+def import_from_url(
+    url: str, fname: Optional[PathLike] = None, *args: Any, **kwargs: Any
+) -> None:
+    """Wrap download so that it works with Kaggle and GitHub."""
+    parsed_url = urlparse(url)
+    path = Path(fname if fname is not None else url.split("/")[-1])
 
-    The result satisfies: ``n = sum(d*b**k for k, d in enumerate(reversed(digits)))``
-
-    References
-    ----------
-    - https://stackoverflow.com/a/28666223/9318372
-    """
-    digits = []
-    while n:
-        n, d = divmod(n, b)
-        digits.append(d)
-    return digits[::-1] or [0]
-
-
-def to_alphanumeric(n: int) -> str:
-    r"""Convert integer to alphanumeric code."""
-    chars = string.ascii_uppercase + string.digits
-    digits = to_base(n, len(chars))
-    return "".join(chars[i] for i in digits)
+    if parsed_url.netloc == "www.kaggle.com":
+        kaggle_name = Path(parsed_url.path).name
+        subprocess.run(
+            f"kaggle competitions download -p {str(path)} -c {kaggle_name}",
+            shell=True,
+            check=True,
+        )
+    elif parsed_url.netloc == "github.com":
+        subprocess.run(
+            f"svn export --force {url.replace('tree/main', 'trunk')} {str(path)}",
+            shell=True,
+            check=True,
+        )
+    else:  # default parsing, including for UCI dataset
+        download(url, path, *args, **kwargs)
 
 
 # def shorthash(inputs) -> str:
@@ -123,3 +126,77 @@ def to_alphanumeric(n: int) -> str:
 #     encoded = json.dumps(dictionary, sort_keys=True).encode()
 #
 #     return shake_256(inputs).hexdigest(8)
+
+
+# def validate(
+#     self,
+#     filespec: Nested[str | Path],
+#     /,
+#     *,
+#     reference: Optional[str | Mapping[str, str]] = None,
+# ) -> None:
+#     r"""Validate the file hash."""
+#     self.LOGGER.debug("Starting to validate dataset")
+#
+#     if isinstance(filespec, Mapping):
+#         for value in filespec.values():
+#             self.validate(value, reference=reference)
+#         return
+#
+#     if isinstance(filespec, Sequence) and not isinstance(filespec, (str, Path)):
+#         for value in filespec:
+#             self.validate(value, reference=reference)
+#         return
+#
+#     assert isinstance(filespec, (str, Path)), f"{filespec=} wrong type!"
+#     file = Path(filespec)
+#
+#     if not file.exists():
+#         raise FileNotFoundError(f"File {file.name!r} does not exist!")
+#
+#     filehash = sha256(file.read_bytes()).hexdigest()
+#
+#     if reference is None:
+#         warnings.warn(
+#             f"File {file.name!r} cannot be validated as no hash is stored in {self.__class__}."
+#             f"The filehash is {filehash!r}."
+#         )
+#
+#     elif isinstance(reference, str):
+#         if filehash != reference:
+#             warnings.warn(
+#                 f"File {file.name!r} failed to validate!"
+#                 f"File hash {filehash!r} does not match reference {reference!r}."
+#                 f"𝗜𝗴𝗻𝗼𝗿𝗲 𝘁𝗵𝗶𝘀 𝘄𝗮𝗿𝗻𝗶𝗻𝗴 𝗶𝗳 𝘁𝗵𝗲 𝗳𝗶𝗹𝗲 𝗳𝗼𝗿𝗺𝗮𝘁 𝗶𝘀 𝗽𝗮𝗿𝗾𝘂𝗲𝘁."
+#             )
+#         self.LOGGER.info(
+#             f"File {file.name!r} validated successfully {filehash=!r}."
+#         )
+#
+#     elif isinstance(reference, Mapping):
+#         if not (file.name in reference) ^ (file.stem in reference):
+#             warnings.warn(
+#                 f"File {file.name!r} cannot be validated as it is not contained in {reference}."
+#                 f"The filehash is {filehash!r}."
+#                 f"𝗜𝗴𝗻𝗼𝗿𝗲 𝘁𝗵𝗶𝘀 𝘄𝗮𝗿𝗻𝗶𝗻𝗴 𝗶𝗳 𝘁𝗵𝗲 𝗳𝗶𝗹𝗲 𝗳𝗼𝗿𝗺𝗮𝘁 𝗶𝘀 𝗽𝗮𝗿𝗾𝘂𝗲𝘁."
+#             )
+#         elif file.name in reference and filehash != reference[file.name]:
+#             warnings.warn(
+#                 f"File {file.name!r} failed to validate!"
+#                 f"File hash {filehash!r} does not match reference {reference[file.name]!r}."
+#                 f"𝗜𝗴𝗻𝗼𝗿𝗲 𝘁𝗵𝗶𝘀 𝘄𝗮𝗿𝗻𝗶𝗻𝗴 𝗶𝗳 𝘁𝗵𝗲 𝗳𝗶𝗹𝗲 𝗳𝗼𝗿𝗺𝗮𝘁 𝗶𝘀 𝗽𝗮𝗿𝗾𝘂𝗲𝘁."
+#             )
+#         elif file.stem in reference and filehash != reference[file.stem]:
+#             warnings.warn(
+#                 f"File {file.name!r} failed to validate!"
+#                 f"File hash {filehash!r} does not match reference {reference[file.stem]!r}."
+#                 f"𝗜𝗴𝗻𝗼𝗿𝗲 𝘁𝗵𝗶𝘀 𝘄𝗮𝗿𝗻𝗶𝗻𝗴 𝗶𝗳 𝘁𝗵𝗲 𝗳𝗶𝗹𝗲 𝗳𝗼𝗿𝗺𝗮𝘁 𝗶𝘀 𝗽𝗮𝗿𝗾𝘂𝗲𝘁."
+#             )
+#         else:
+#             self.LOGGER.info(
+#                 f"File {file.name!r} validated successfully {filehash=!r}."
+#             )
+#     else:
+#         raise TypeError(f"Unsupported type for {reference=}.")
+#
+#     self.LOGGER.debug("Finished validating file.")
