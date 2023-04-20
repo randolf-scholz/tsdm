b55478f4cfd5271ddea841b85b9f17a3172ae722
Randolf Scholz <randolf.scholz@gmail.com>
2023-04-20 17:42:22 +0200

    updated hash algorithms

--- src/tsdm/utils/hash.py
+++ src/tsdm/utils/hash.py
@@ -11,6 +11,7 @@ __all__ = [
     "to_alphanumeric",
     "to_base",
     "validate_file",
+    "validate_array",
 ]
 
 import hashlib
@@ -20,9 +21,11 @@ import warnings
 from collections import Counter
 from collections.abc import Hashable, Iterable, Mapping
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal, Optional
 
-import pandas
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
 from pandas import DataFrame, Index, MultiIndex, Series
 
 from tsdm.types.aliases import PathLike
@@ -109,13 +112,21 @@ def hash_pandas(
     if col_invariant:
         df = df.stack()
 
-    row_hash = pandas.util.hash_pandas_object(df, index=index)
+    row_hash = pd.util.hash_pandas_object(df, index=index)
 
     if row_invariant:
         hash_value = hash_set(row_hash, ignore_duplicates=ignore_duplicates)
     else:
         hash_value = hash(tuple(row_hash))
 
+    return hash_value
+
+
+def hash_numpy(array: NDArray, /) -> int:
+    """Hash a numpy array."""
+    # TODO: there are probably better ways to hash numpy arrays.
+    array = np.asarray(array)
+    hash_value = hash(tuple(array.flatten()))
     return hash_value
 
 
@@ -137,22 +148,87 @@ def hash_file(
     return hash_value.hexdigest()
 
 
+def hash_array(
+    array: Any, *, hash_algorithm: str = "pandas", **hash_kwargs: Any
+) -> int:
+    """Hash an array like object (pandas/numpy/pyarrow/etc)."""
+    if hash_algorithm == "pandas":
+        return hash_pandas(array, **hash_kwargs)
+    elif hash_algorithm == "numpy":
+        return hash_numpy(array, **hash_kwargs)
+
+    raise NotImplementedError(f"No algorithm {hash_algorithm!r} known.")
+
+
 def validate_file(
-    file: PathLike,
-    reference_hash: str | None = None,
+    file: PathLike | Mapping[str, PathLike],
+    reference_hash: None | str | Mapping[str, tuple[str, str]] = None,
     *,
     hash_algorithm: str | None = None,
-    hash_table: Mapping[str, tuple[str, str]] | None = None,
     logger: logging.Logger = __logger__,
+    errors: Literal["warn", "raise", "ignore"] = "warn",
     **hash_kwargs: Any,
 ) -> None:
-    """Validate the file hash, given hash values from a table."""
+    """Validate file(s), given reference hash value(s).
+
+    Arguments:
+        file: The file to validate. If a mapping is given, every file in the mapping is validated.
+        reference_hash: The reference hash value, or a mapping from file names to
+            (hash_algorithm, hash_value) pairs.
+        hash_algorithm: The hash algorithm to use, if no reference hash is given.
+        logger: Optional logger to use.
+        errors: How to handle errors. Can be "warn", "raise", or "ignore".
+        hash_kwargs: Additional keyword arguments to pass to the hash function.
+
+    Raises:
+        ValueError: If the file hash does not match the reference hash.
+        LookupError: If the file is not found in the reference hash table.
+    """
+    if errors not in {"warn", "raise", "ignore"}:
+        raise ValueError(
+            f"Invalid value for errors: {errors!r}. "
+            "Only 'warn', 'raise', and 'ignore' are allowed."
+        )
+
+    if isinstance(file, Mapping):
+        for key, value in file.items():
+            if isinstance(reference_hash, Mapping):
+                alg, ref = reference_hash.get(key, (None, None))
+                validate_file(
+                    value,
+                    reference_hash=ref,
+                    hash_algorithm=alg,
+                    logger=logger,
+                    errors=errors,
+                    **hash_kwargs,
+                )
+            elif reference_hash is None:
+                validate_file(
+                    value,
+                    reference_hash=reference_hash,
+                    hash_algorithm=hash_algorithm,
+                    logger=logger,
+                    errors=errors,
+                    **hash_kwargs,
+                )
+            else:
+                raise ValueError(
+                    "If a mapping of files is provided, reference_hash must be a mapping as well!"
+                )
+        return
+
+    # code for single path
     file = Path(file)
 
-    if hash_table is not None and reference_hash is not None:
-        raise ValueError("Provide either reference_hash or hash_table, not both.")
+    if file.suffix == ".parquet":
+        warnings.warn(
+            f"Parquet file {file.name!r} not validated since the format is not binary stable!",
+            UserWarning,
+            stacklevel=2,
+        )
 
-    if hash_table is not None:
+    if isinstance(reference_hash, Mapping):
+        hash_table = reference_hash
         hash_algorithm, reference_hash = hash_table.get(
             str(file),
             hash_table.get(file.name, hash_table.get(file.stem, (None, None))),
@@ -162,64 +238,69 @@ def validate_file(
     hash_value = hash_file(file, hash_algorithm=hash_algorithm, **hash_kwargs)
 
     if reference_hash is None:
-        warnings.warn(
+        msg = (
             f"No reference hash given for file {file.name!r}."
-            f"The {hash_algorithm!r}-hash is {hash_value!r}.",
-            RuntimeWarning,
-            stacklevel=2,
+            f"The {hash_algorithm!r}-hash is {hash_value!r}."
         )
+        if errors == "raise":
+            raise LookupError(msg)
+        elif errors == "warn":
+            warnings.warn(msg, UserWarning, stacklevel=2)
+        else:
+            logger.info(msg)
     elif hash_value != reference_hash:
-        warnings.warn(
+        msg = (
             f"File {file.name!r} failed to validate!"
             f"File hash {hash_value!r} does not match reference {reference_hash!r}."
-            f"Ignore this warning if the format is parquet.",
-            RuntimeWarning,
-            stacklevel=2,
+            f"Ignore this warning if the format is parquet."
         )
+        if errors == "raise":
+            raise ValueError(msg)
+        elif errors == "warn":
+            warnings.warn(msg, UserWarning, stacklevel=2)
+        else:
+            logger.info(msg)
     else:
         logger.info(
             "File '%s' validated successfully with '%s'-hash '%s'.",
-            file,
-            hash_algorithm,
-            hash_value,
+            *(file, hash_algorithm, hash_value),
         )
 
 
-def validatate_pandas(
-    df: DataFrame,
-    reference_hash: str | None = None,
+def validate_array(
+    array: Any,
+    reference_hash: str | None,
     *,
-    hash_algorithm: str | None = None,
-    hash_table: Mapping[str, tuple[str, str]] | None = None,
+    hash_algorithm: Optional[str] = None,
     logger: logging.Logger = __logger__,
     **hash_kwargs: Any,
 ) -> None:
     """Validate the hash of a pandas object, given hash values from a table."""
-    if hash_table is not None and reference_hash is not None:
-        raise ValueError("Provide either reference_hash or hash_table, not both.")
+    # Try to determine the hash algorithm from the array type
+    if hash_algorithm is None:
+        match array:
+            case pd.DataFrame() | pd.Series() | pd.Index():
+                hash_algorithm = "pandas"
+            case np.ndarray():
+                hash_algorithm = "numpy"
+            case _:
+                raise ValueError(
+                    f"Cannot determine hash algorithm for array-like object {array!r}."
+                    "Please specify the hash algorithm manually."
+                )
 
-    if not hasattr(df, "name"):
-        raise ValueError("Pandas object must have a name attribute.")
-
-    if hash_table is not None:
-        hash_algorithm, reference_hash = hash_table.get(
-            df.name,
-            hash_table.get(df.name, hash_table.get(df.name, (None, None))),
-        )  # try to match by full path, then by name, then by stem
-
-    hash_algorithm = "sha256" if hash_algorithm is None else hash_algorithm
-    hash_value = hash_pandas(df, hash_algorithm=hash_algorithm, **hash_kwargs)
-
+    hash_value = hash_array(array, hash_algorithm=hash_algorithm, **hash_kwargs)
+    name = f"{type(array)}<{array.shape}> {id(array)}"
     if reference_hash is None:
         warnings.warn(
-            f"No reference hash given for pandas object {df.name!r}."
+            f"No reference hash given for array-like object {name!r}."
             f"The {hash_algorithm!r}-hash is {hash_value!r}.",
             RuntimeWarning,
             stacklevel=2,
         )
     elif hash_value != reference_hash:
         warnings.warn(
-            f"Pandas object {df.name!r} failed to validate!"
+            f"Array-Like  object {name!r} failed to validate!"
             f"Hash {hash_value!r} does not match reference {reference_hash!r}."
             f"Ignore this warning if the format is parquet.",
             RuntimeWarning,
@@ -227,8 +308,8 @@ def validatate_pandas(
         )
     else:
         logger.info(
-            "Pandas object '%s' validated successfully with '%s'-hash '%s'.",
-            df,
+            "Array-Like object '%s' validated successfully with '%s'-hash '%s'.",
+            name,
             hash_algorithm,
             hash_value,
         )
