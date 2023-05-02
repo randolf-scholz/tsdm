b46654f951132dbca062f7e998f0f5ca314f7037
Randolf Scholz <randolf.scholz@gmail.com>
2023-05-02 19:47:13 +0200

    InSilico-Dataset outliers

--- src/tsdm/datasets/in_silico_data.py
+++ src/tsdm/datasets/in_silico_data.py
@@ -3,7 +3,6 @@ r"""In silico experiments."""
 __all__ = ["InSilicoData"]
 
 import shutil
-from functools import cached_property
 from importlib import resources
 from zipfile import ZipFile
 
@@ -12,6 +11,7 @@ from pandas import DataFrame
 
 from tsdm.datasets import examples
 from tsdm.datasets.base import SingleTableDataset
+from tsdm.utils.data import remove_outliers
 
 
 class InSilicoData(SingleTableDataset[DataFrame]):
@@ -28,26 +28,53 @@ class InSilicoData(SingleTableDataset[DataFrame]):
     +---------+---------+---------+-----------+---------+-------+---------+-----------+------+
     """
 
-    dataset_hash = (
-        "sha256:f6938b4e9de35824c24c3bdc7f08c4d9bfcf9272eaeb76f579d823ca8628bff0"
-    )
-    table_shape = (5206, 7)
-    # table_hash = "652930435272677160"
-
     rawdata_files = ["in_silico.zip"]
     rawdata_hashes = {
         "in_silico.zip": "sha256:ee9ad6278fb27dd933c22aecfc7b5b2501336e859a7f012cace2bb265f713cba",
     }
+    table_shapes = {"timeseries": (5206, 7)}
+    table_schemas = {
+        "timeseries": {
+            # fmt: off
+            "Biomass"   : "float[pyarrow]",
+            "Substrate" : "float[pyarrow]",
+            "Acetate"   : "float[pyarrow]",
+            "DOTm"      : "float[pyarrow]",
+            "Product"   : "float[pyarrow]",
+            "Volume"    : "float[pyarrow]",
+            "Feed"      : "float[pyarrow]",
+            # fmt: on
+        },
+        "timeseries_description": {
+            # fmt: off
+            "variable"       : "string[pyarrow]",
+            "lower"          : "float32[pyarrow]",
+            "upper"          : "float32[pyarrow]",
+            "lower_included" : "bool[pyarrow]",
+            "upper_included" : "bool[pyarrow]",
+            "unit"           : "string[pyarrow]",
+            "description"    : "string[pyarrow]",
+            # fmt: on
+        },
+    }
 
-    @cached_property
-    def index(self) -> pd.Index:
-        r"""Return the index of the dataset."""
-        return self.timeseries.index.get_level_values(0).unique()
-
-    @cached_property
-    def timeseries(self) -> pd.DataFrame:
-        r"""Return the timeseries of the dataset."""
-        return self.table
+    def _timeseries_description(self) -> DataFrame:
+        data = [
+            # fmt: off
+            ("Biomass"  , 0, None, True, True, "g/L", None),
+            ("Substrate", 0, None, True, True, "g/L", None),
+            ("Acetate"  , 0, None, True, True, "g/L", None),
+            ("DOTm"     , 0, 1,    True, True, "%",   None),
+            ("Product"  , 0, None, True, True, "g/L", None),
+            ("Volume"   , 0, None, True, True, "L",   None),
+            ("Feed"     , 0, None, True, True, "μL",  None),
+            # fmt: on
+        ]
+        return (
+            DataFrame(data, columns=list(self.table_schemas["timeseries_description"]))
+            .astype(self.table_schemas["timeseries_description"])
+            .set_index("variable")
+        )
 
     def clean_table(self) -> DataFrame:
         with ZipFile(self.rawdata_paths["in_silico.zip"]) as files:
@@ -59,14 +86,15 @@ class InSilicoData(SingleTableDataset[DataFrame]):
                     dfs[key] = df.rename_axis(index="time")
 
         # Set index, dtype and sort.
-        ds = (
+        ts = (
             pd.concat(dfs, names=["run_id"])
             .reset_index()
             .set_index(["run_id", "time"])
             .sort_index()
-            .astype("Float32")
+            .astype("float32[pyarrow]")
         )
-        return ds
+        ts = remove_outliers(ts, self._timeseries_description())
+        return ts
 
     def download_file(self, fname: str) -> None:
         r"""Download the dataset."""
