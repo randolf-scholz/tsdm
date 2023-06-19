fixed numerical encoders

src/tsdm/tasks/_deprecated_electricity_lim2021.py
@@ -18,7 +18,7 @@ from torch import Tensor
 from torch.utils.data import DataLoader, TensorDataset
 
 from tsdm.datasets import Electricity
-from tsdm.encoders import Encoder, Standardizer
+from tsdm.encoders import Encoder, StandardScaler
 from tsdm.random.samplers import SequenceSampler
 from tsdm.tasks._deprecated import OldBaseTask
 from tsdm.utils.strings import repr_namedtuple
@@ -129,7 +129,7 @@ class ElectricityLim2021(OldBaseTask):
         self.observation_horizon = 24 * 7
         self.forecasting_horizon = 24
 
-        self.encoder = Standardizer()
+        self.encoder = StandardScaler()
         self.encoder.fit(self.splits["train"])
 
     @cached_property

src/tsdm/tasks/mimic_iv_bilos2021.py
@@ -23,7 +23,7 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset
 
 from tsdm.datasets import MIMIC_IV_Bilos2021 as MIMIC_IV_Dataset
-from tsdm.encoders import FrameEncoder, MinMaxScaler, Standardizer
+from tsdm.encoders import FrameEncoder, MinMaxScaler, StandardScaler
 from tsdm.tasks._deprecated import OldBaseTask
 from tsdm.utils import is_partition
 from tsdm.utils.strings import repr_namedtuple
@@ -186,12 +186,12 @@ class MIMIC_IV_Bilos2021(OldBaseTask):
     test_size = 0.15  # of total
     valid_size = 0.2  # of train split size, i.e. 0.85*0.2=0.17
 
-    preprocessor: FrameEncoder[Standardizer, dict[Any, MinMaxScaler]]
+    preprocessor: FrameEncoder[StandardScaler, dict[Any, MinMaxScaler]]
 
     def __init__(self, *, normalize_time: bool = True) -> None:
         super().__init__()
         self.preprocessor = FrameEncoder(
-            column_encoders=Standardizer(),
+            column_encoders=StandardScaler(),
             index_encoders={"time_stamp": MinMaxScaler()},
         )
         self.normalize_time = normalize_time
@@ -211,7 +211,7 @@ class MIMIC_IV_Bilos2021(OldBaseTask):
         self.preprocessor.fit(ts)
         ts = self.preprocessor.encode(ts)
         index_encoder = self.preprocessor.index_encoders["time_stamp"]
-        self.observation_time /= index_encoder.param.xmax  # type: ignore[assignment]
+        self.observation_time /= index_encoder.params.xmax  # type: ignore[assignment]
 
         # drop values outside 5 sigma range
         ts = ts[(-5 < ts) & (ts < 5)]

tests/tsdm/encoders/test_standardizer.py
@@ -7,13 +7,13 @@ import numpy as np
 import torch
 from pytest import mark
 
-from tsdm.encoders import MinMaxScaler, Standardizer
+from tsdm.encoders import MinMaxScaler, StandardScaler
 
 logging.basicConfig(level=logging.INFO)
 __logger__ = logging.getLogger(__name__)
 
 
-@mark.parametrize("Encoder", (Standardizer, MinMaxScaler))
+@mark.parametrize("Encoder", (StandardScaler, MinMaxScaler))
 @mark.parametrize("tensor_type", (np.array, torch.tensor))
 def test_standardizer(Encoder, tensor_type):
     r"""Check whether the Standardizer encoder works as expected."""
@@ -70,7 +70,7 @@ def test_standardizer(Encoder, tensor_type):
 
 
 def _main() -> None:
-    test_standardizer(Standardizer, np.array)
+    test_standardizer(StandardScaler, np.array)
     test_standardizer(MinMaxScaler, np.array)
 
 