bce0e8b01388f26893ad2b866347ec713dc759e7
Randolf Scholz <randolf.scholz@gmail.com>
2023-10-01 21:49:39 +0200

    torch bug

--- bugs/torch/optim_bug.py
+++ bugs/torch/optim_bug.py
@@ -5,9 +5,16 @@ from torch import Tensor, jit, nn
 device = torch.device("cuda")
 model = nn.Linear(4, 4).to(device=device)
 
+# optimizer_config = {
+#     "lr": 0.001,
+#     "betas": torch.tensor([0.9000, 0.9990]),
+# }
+
 optimizer_config = {
     "lr": 0.001,
     "betas": torch.tensor([0.9000, 0.9990]),
+    "foreach": False,
+    # "fused": False,
 }
 
 optim = torch.optim.Adam(model.parameters(), **optimizer_config)
