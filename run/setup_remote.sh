fca184404b1ac3728889db2f017818874900b603
Randolf Scholz <randolf.scholz@gmail.com>
2023-10-19 11:08:06 +0200

    updated remote setup

--- run/setup_remote.sh
+++ run/setup_remote.sh
@@ -2,27 +2,21 @@
 echo -e "\n" "Current remotes:"
 git remote -v
 
+# remotes
+BERLIN="https://git.tu-berlin.de/bvt-htbd/kiwi/tf1/tsdm.git"
+HILDESHEIM="https://software.ismll.uni-hildesheim.de/ISMLL-internal/time-series/tsdm.git"
+GITHUB="https://github.com/randolf-scholz/tsdm.git"
+
 echo -e "\nDeleting all remotes..."
 for remote_name in $(git remote); do
     git remote remove "${remote_name}"
 done
 
-BERLIN="https://git.tu-berlin.de/bvt-htbd/kiwi/tf1/tsdm.git"
-HILDESHEIM="https://software.ismll.uni-hildesheim.de/ISMLL-internal/time-series/tsdm.git"
-GITHUB="https://github.com/randolf-scholz/tsdm.git"
-
 echo -e "\nAdding remote ${BERLIN}..."
 git remote add berlin $BERLIN
 git remote set-url --add --push berlin $BERLIN
-git remote set-url --add --push berlin $HILDESHEIM
 git remote set-url --add --push berlin $GITHUB
-
-
-echo -e "\nAdding remote ${HILDESHEIM}..."
-git remote add hildesheim $HILDESHEIM
-git remote set-url --add --push hildesheim $HILDESHEIM
-git remote set-url --add --push hildesheim $GITHUB
-git remote set-url --add --push hildesheim $BERLIN
+git remote set-url --add --push berlin $HILDESHEIM
 
 echo -e "\nAdding remote ${GITHUB}..."
 git remote add github $GITHUB
@@ -30,6 +24,11 @@ git remote set-url --add --push github $GITHUB
 git remote set-url --add --push github $BERLIN
 git remote set-url --add --push github $HILDESHEIM
 
+echo -e "\nAdding remote ${HILDESHEIM}..."
+git remote add hildesheim $HILDESHEIM
+git remote set-url --add --push hildesheim $HILDESHEIM
+git remote set-url --add --push hildesheim $BERLIN
+git remote set-url --add --push hildesheim $GITHUB
 
 echo -e "\nSetting default remote:"
 git fetch berlin
