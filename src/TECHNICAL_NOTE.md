3ca85204e9ce6003a998d35cece283bef5bed762
Randolf Scholz <randolf.scholz@gmail.com>
2024-04-02 13:40:47 +0200

    updated kiwi-schema

--- src/TECHNICAL_NOTE.md
+++ src/TECHNICAL_NOTE.md
@@ -14,3 +14,23 @@
 - `self.make_*`-methods: return a new object that's expensive to compute / doesn't exist yet
 - `cls.from_*`-methods: classmethods that return a new object from data
   - must return a `Self`.
+
+## Timeseries Descriptions
+
+For a given table, we want to describe the variables, i.e. columns of the table.
+We consider several attributes of interest:
+
+| name        | dtype      | description                                     | examples                                                     |
+|-------------|------------|-------------------------------------------------|--------------------------------------------------------------|
+| dtype       | `cat[str]` | The data-type associated with the variable      | `int32`, `float64`, `cat[str]`                               |
+| unit        | `cat[str]` | The physical unit associated with the varaible. | `kg`, `%`, `‰`                                               |
+| type        | `cat[str]` | Arbitrary category to assign to the variable    | `control`, `observable`                                      |
+| scale       | `cat[str]` | The scale the variable lives on.                | `absolute`, `linear`, `logarithmic`, `percentage`            |
+| bounds      | `interval` | The range of valid values of the variable.      | `[0,14]` (pH), `[0,∞)`: weight, `[−273.15,∞)` (temp in `°C`) |
+| description | `str`      | Description of the variable.                    |                                                              |
+
+- `dtype (cat[str])`: The data-type associated with the variable
+- `unit (cat[str])`: The unit of the variable. **Note:** dimensionless quantities should be represented by the unit `1`.
+- `type (cat[str])`: The type of the variable (arbitrary classification).
+- `description (str)`: A description of the variable
+- `scale (cat[str])`: The scale of the variable (applicable to real-valued variables)
--- src/tsdm/datasets/kiwi/kiwi_schema.py
+++ src/tsdm/datasets/kiwi/kiwi_schema.py
@@ -6,21 +6,21 @@ __all__ = ["timeseries_description", "metadata_description"]
 
 timeseries_description = {
     # Name                          : [Unit,     Type,  Dtype, Lower, Upper, Lower included, Upper included]
-    "Acetate"                       : ["%",      "percent",  "float32", 0,   100,       True, True],
-    "Base"                          : ["uL",     "absolute", "float32", 0,   None,      True, True],
-    "Cumulated_feed_volume_glucose" : ["uL",     "absolute", "float32", 0,   None,      True, True],
-    "Cumulated_feed_volume_medium"  : ["uL",     "absolute", "float32", 0,   None,      True, True],
-    "DOT"                           : ["%",      "percent",  "float32", 0,   100,       True, True],
-    "Flow_Air"                      : ["Ln/min", "absolute", "float32", 0,   None,      True, True],
-    "Fluo_GFP"                      : ["RFU",    "absolute", "float32", 0,   1_000_000, True, True],
-    "Glucose"                       : ["g/L",    "absolute", "float32", 0,   20,        True, True],
-    "InducerConcentration"          : ["mM",     "absolute", "float32", 0,   None,      True, True],
-    "OD600"                         : ["%",      "percent",  "float32", 0,   100,       True, True],
-    "Probe_Volume"                  : ["uL",     "absolute", "float32", 0,   None,      True, True],
-    "StirringSpeed"                 : ["U/min",  "absolute", "float32", 0,   None,      True, True],
-    "Temperature"                   : ["°C",     "linear",   "float32", 20,  45,        True, True],
-    "Volume"                        : ["mL",     "absolute", "float32", 0,   None,      True, True],
-    "pH"                            : ["pH",     "linear",   "float32", 4,   10,        True, True],
+    "Acetate"                       : ["g/L"    , "absolute" , "float32" , 0  , None         , True , True],
+    "Base"                          : ["uL"     , "absolute" , "float32" , 0  , None      , True , True],
+    "Cumulated_feed_volume_glucose" : ["uL"     , "absolute" , "float32" , 0  , None      , True , True],
+    "Cumulated_feed_volume_medium"  : ["uL"     , "absolute" , "float32" , 0  , None      , True , True],
+    "DOT"                           : ["%"      , "percent"  , "float32" , 0  , 100       , True , True],
+    "Flow_Air"                      : ["Ln/min" , "absolute" , "float32" , 0  , None      , True , True],
+    "Fluo_GFP"                      : ["RFU"    , "absolute" , "float32" , 0  , 1_000_000 , True , True],
+    "Glucose"                       : ["g/L"    , "absolute" , "float32" , 0  , 20        , True , True],
+    "InducerConcentration"          : ["mM"     , "absolute" , "float32" , 0  , None      , True , True],
+    "OD600"                         : ["%"      , "percent"  , "float32" , 0  , 100       , True , True],
+    "Probe_Volume"                  : ["uL"     , "absolute" , "float32" , 0  , None      , True , True],
+    "StirringSpeed"                 : ["U/min"  , "absolute" , "float32" , 0  , None      , True , True],
+    "Temperature"                   : ["°C"     , "linear"   , "float32" , 20 , 45        , True , True],
+    "Volume"                        : ["mL"     , "absolute" , "float32" , 0  , None      , True , True],
+    "pH"                            : ["pH"     , "linear"   , "float32" , 4  , 10        , True , True],
 }  # fmt: skip
 
 metadata_description = {
