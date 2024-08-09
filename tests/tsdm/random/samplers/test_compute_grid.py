39752e4a21ecbf5e76bd122def7ceb9f3391d9e2
Randolf Scholz <randolf.scholz@gmail.com>
2024-08-09 17:44:40 +0200

    fixed failing test

--- tests/tsdm/random/samplers/test_compute_grid.py
+++ tests/tsdm/random/samplers/test_compute_grid.py
@@ -19,10 +19,9 @@ from tsdm.types.time import DateTime, TimeDelta
 from tsdm.utils import timedelta as pd_td, timestamp as pd_dt
 
 __logger__ = logging.getLogger(__name__)
-MODES = ["numpy", "pandas", "python", "np_int", "np_float", "int", "float"]
 
 
-def _validate_grid_results(tmin, tmax, tdelta, offset):
+def validate_grid_results(*, tmin, tmax, tdelta, offset):
     result = compute_grid(tmin, tmax, tdelta, offset=offset)
     kmin, kmax = result[0], result[-1]
 
@@ -57,94 +56,91 @@ class GridTuple[DT: DateTime, TD: TimeDelta](NamedTuple):
     timedelta: TD
 
 
-def _make_inputs(mode: str) -> GridTuple[DateTime, TimeDelta]:
-    match mode:
-        case "numpy":
-            # noinspection PyArgumentList
-            return GridTuple(
-                tmin=np_dt("2000-01-01"),
-                tmax=np_dt("2001-01-01"),
-                offset=np_dt("2000-01-15"),
-                timedelta=np_td(1, "h"),
-            )
-        case "pandas":
-            # noinspection PyArgumentList
-            return GridTuple(
-                tmin=pd_dt("2000-01-01"),
-                tmax=pd_dt("2001-01-01"),
-                offset=pd_dt("2000-01-15"),
-                timedelta=pd_td("1h"),
-            )
-        case "python":
-            # noinspection PyArgumentList
-            return GridTuple(
-                tmin=py_dt(2000, 1, 1),
-                tmax=py_dt(2001, 1, 1),
-                offset=py_dt(2000, 1, 15),
-                timedelta=py_td(hours=1),
-            )
-        case "np_int":
-            # noinspection PyArgumentList
-            return GridTuple(
-                tmin=np_int(0),
-                tmax=np_int(100),
-                offset=np_int(1),
-                timedelta=np_int(1),  # type: ignore[arg-type]
-            )
-        case "np_float":
-            # noinspection PyArgumentList
-            return GridTuple(
-                tmin=np_float(0.0),
-                tmax=np_float(99.9),
-                offset=np_float(1.4),
-                timedelta=np_float(0.6),  # type: ignore[arg-type]
-            )
-        case "int":
-            # noinspection PyArgumentList
-            return GridTuple(
-                tmin=0,
-                tmax=100,
-                offset=1,
-                timedelta=1,
-            )
-        case "float":
-            # noinspection PyArgumentList
-            return GridTuple(
-                tmin=0.0,
-                tmax=99.9,
-                offset=0.6,
-                timedelta=1.4,
-            )
-        case _:
-            raise ValueError(f"Unknown mode {mode=}")
+EXAMPLES: dict[str, GridTuple[DateTime, TimeDelta]] = {
+    "python_datetime": GridTuple(
+        tmin=py_dt(2000, 1, 1),
+        tmax=py_dt(2001, 1, 1),
+        offset=py_dt(2000, 1, 15),
+        timedelta=py_td(hours=1),
+    ),
+    "python_int": GridTuple(
+        tmin=0,
+        tmax=100,
+        offset=1,
+        timedelta=1,
+    ),
+    "python_float": GridTuple(
+        tmin=0.0,
+        tmax=99.9,
+        offset=0.6,
+        timedelta=1.4,
+    ),
+    "pandas_datetime": GridTuple(
+        tmin=pd_dt("2000-01-01"),
+        tmax=pd_dt("2001-01-01"),
+        offset=pd_dt("2000-01-15"),
+        timedelta=pd_td("1h"),
+    ),
+    "numpy_datetime": GridTuple(
+        tmin=np_dt("2000-01-01"),
+        tmax=np_dt("2001-01-01"),
+        offset=np_dt("2000-01-15"),
+        timedelta=np_td(1, "h"),
+    ),
+    "numpy_float": GridTuple(
+        tmin=np_float(0.0),
+        tmax=np_float(99.9),
+        offset=np_float(1.4),
+        timedelta=np_float(0.6),  # type: ignore[arg-type]
+    ),
+    "numpy_int": GridTuple(
+        tmin=np_int(0),
+        tmax=np_int(100),
+        offset=np_int(1),
+        timedelta=np_int(1),  # type: ignore[arg-type]
+    ),
+}
 
 
 def test_edge_case() -> None:
+    r"""Test edge case.
+
+    In this example, The following windows are considerable:
+    [11, 12, 13, 14]  (initial window)
+    [13, 14, 15, 16]  (shifted once)
+    [15, 16, 17, 18]  (shifted twice)
+    [17, 18, 19, 20]  (shifted thrice)
+
+    However, the last window is excluded, despite being within the range.
+    This is because compute grid checks for the true inequality `t_last < t_max`.
+
+    This is the correct thing to do for continuous data, but not for discrete data.
+    """
     data = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     stride = 2
     tmin = min(data)
     tmax = max(data)
     horizon = 4
     offset = tmin + horizon
-    print((tmin - offset) / stride, math.ceil((tmin - offset) / stride))  # -2.0
-    print((tmax - offset) / stride, math.floor((tmax - offset) / stride))  # 2.5
-    print((tmax - tmin - horizon) / stride)
-
+    assert (tmin - offset) / stride == -2.0
+    assert math.ceil((tmin - offset) / stride) == -2
+    assert (tmax - offset) / stride == +2.5
+    assert math.floor((tmax - offset) / stride) == +2
+    assert (tmax - tmin - horizon) / stride == 2.5
     grid = compute_grid(tmin - tmin, tmax - tmin - horizon, stride)
-    print(grid)
+    assert grid == [0, 1, 2]
 
 
-@pytest.mark.parametrize("mode", MODES)
-def test_grid_pandas(mode: str) -> None:
+@pytest.mark.parametrize("name", EXAMPLES)
+def test_grid_pandas(name: str) -> None:
     r"""Test compute_grid function with various input types."""
-    LOGGER = __logger__.getChild(compute_grid.__name__)
-    LOGGER.info("Testing mode: %s", mode)
-
-    tmin, tmax, timedelta, offset = _make_inputs(mode)
-
-    _validate_grid_results(tmin, tmax, timedelta, offset)
-
-    LOGGER.info("Finished testing mode: %s", mode)
+    inputs = EXAMPLES[name]
+    validate_grid_results(
+        tmin=inputs.tmin,
+        tmax=inputs.tmax,
+        tdelta=inputs.timedelta,
+        offset=inputs.offset,
+    )
 
 
 def test_grid_extra() -> None:
@@ -157,6 +153,6 @@ def test_grid_extra() -> None:
     td = pd_td("15m")
     offset = tmin + td
 
-    _validate_grid_results(tmin, tmax, td, offset)
+    validate_grid_results(tmin=tmin, tmax=tmax, tdelta=td, offset=offset)
 
     LOGGER.info("Finished testing on extra data")
