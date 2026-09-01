b92753c289f4719f18039a5efe02718df0a993dd
Randolf Scholz <randolf.scholz@gmail.com>
2026-09-01 17:38:58 +0200

    refactor: reorganize `datatools` submodules; simplify imports and consolidate `__all__` definitions

--- src/tsdm/datatools/__init__.py
+++ src/tsdm/datatools/__init__.py
@@ -2,72 +2,20 @@ r"""Subclasses of `torch.util.data.Dataset`."""
 
 __all__ = [
     # Modules
-    # "timeseries",
-    # Protocols
-    "CallableDataset",
-    "MapDataset",
-    "PandasDataset",
-    "Dataset",
-    # Functions
-    # collections
-    "get_first_sample",
-    "get_index",
-    "get_last_sample",
-    # preprocessing
-    "aggregate_nondestructive",
-    "get_integer_cols",
-    "is_integer_series",
-    "remove_outliers",
-    "select_outliers",
-    "strip_whitespace",
-    # folds
-    "folds_as_frame",
-    "folds_as_sparse_frame",
-    "folds_from_groups",
-    "is_partition",
-    # utils
-    "data_overview",
-    "date_range",
-    "describe",
-    "get_dtypes",
-    "get_schema",
-    "timedelta",
-    "timedelta_range",
-    "timestamp",
-    "validate_schema",
+    "collections",
+    "preprocessing",
+    "utils",
+    "folds",
 ]
+from . import collections, folds, preprocessing, serialize, utils
+from .collections import *  # ruff: ignore[F403]
+from .folds import *  # ruff: ignore[F403]
+from .preprocessing import *  # ruff: ignore[F403]
+from .serialize import *  # ruff: ignore[F403]
+from .utils import *  # ruff: ignore[F403]
 
-from .collections import (
-    CallableDataset,
-    Dataset,
-    MapDataset,
-    PandasDataset,
-    get_first_sample,
-    get_index,
-    get_last_sample,
-)
-from .folds import (
-    folds_as_frame,
-    folds_as_sparse_frame,
-    folds_from_groups,
-    is_partition,
-)
-from .preprocessing import (
-    aggregate_nondestructive,
-    get_integer_cols,
-    is_integer_series,
-    remove_outliers,
-    select_outliers,
-    strip_whitespace,
-)
-from .utils import (
-    data_overview,
-    date_range,
-    describe,
-    get_dtypes,
-    get_schema,
-    timedelta,
-    timedelta_range,
-    timestamp,
-    validate_schema,
-)
+__all__ += collections.__all__
+__all__ += folds.__all__
+__all__ += preprocessing.__all__
+__all__ += serialize.__all__
+__all__ += utils.__all__
