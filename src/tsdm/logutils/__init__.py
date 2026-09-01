013bd687c73b788266de0dcd9df123e656ee7501
Randolf Scholz <randolf.scholz@gmail.com>
2026-09-01 18:01:45 +0200

    refactor: simplify imports in `logutils` and consolidate `__all__` definitions

--- src/tsdm/logutils/__init__.py
+++ src/tsdm/logutils/__init__.py
@@ -10,113 +10,53 @@ __all__ = [
     "LOGGERS",
     "LOGFUNCS",
     "CALLBACKS",
-    # ABCs & Protocols
-    "BaseCallback",
-    "BaseLogger",
-    "Callback",
-    "CallbackSequence",
-    "DefaultLogger",
-    "LogFunction",
-    "Logger",
-    # Classes
-    "CallbackList",
-    "CheckpointCallback",
-    "ConfigCallback",
-    "EvaluationCallback",
-    "HParamCallback",
-    "KernelCallback",
-    "LRSchedulerCallback",
-    "MetricsCallback",
-    "ModelCallback",
-    "OptimizerCallback",
-    "ScalarsCallback",
-    "TableCallback",
-    "WrapCallback",
-    # Functions
-    "is_callback",
-    "is_logfunc",
-    "log_config",
-    "log_kernel",
-    "log_lr_scheduler",
-    "log_metrics",
-    "log_model",
-    "log_optimizer",
-    "log_plot",
-    "log_table",
-    "log_values",
-    # extras
-    "save_checkpoint",
 ]
 
 from . import callbacks, logfuncs, loggers, utils
-from .callbacks import (
-    BaseCallback,
-    Callback,
-    CallbackList,
-    CallbackSequence,
-    CheckpointCallback,
-    ConfigCallback,
-    EvaluationCallback,
-    HParamCallback,
-    KernelCallback,
-    LRSchedulerCallback,
-    MetricsCallback,
-    ModelCallback,
-    OptimizerCallback,
-    ScalarsCallback,
-    TableCallback,
-    WrapCallback,
-    is_callback,
-)
-from .logfuncs import (
-    LogFunction,
-    is_logfunc,
-    log_config,
-    log_kernel,
-    log_lr_scheduler,
-    log_metrics,
-    log_model,
-    log_optimizer,
-    log_plot,
-    log_table,
-    log_values,
-)
-from .loggers import BaseLogger, DefaultLogger, Logger
-from .utils import save_checkpoint
+from .callbacks import *  # ruff: ignore[F403]
+from .logfuncs import *  # ruff: ignore[F403]
+from .loggers import *  # ruff: ignore[F403]
+from .utils import *  # ruff: ignore[F403]
 
-CALLBACKS: dict[str, type[Callback]] = {
-    "CallbackList"        : CallbackList,
-    "CheckpointCallback"  : CheckpointCallback,
-    "ConfigCallback"      : ConfigCallback,
-    "EvaluationCallback"  : EvaluationCallback,
-    "HParamCallback"      : HParamCallback,
-    "KernelCallback"      : KernelCallback,
-    "LRSchedulerCallback" : LRSchedulerCallback,
-    "MetricsCallback"     : MetricsCallback,
-    "ModelCallback"       : ModelCallback,
-    "OptimizerCallback"   : OptimizerCallback,
-    "ScalarsCallback"     : ScalarsCallback,
-    "TableCallback"       : TableCallback,
-    "WrapCallback"        : WrapCallback,
+__all__ += callbacks.__all__
+__all__ += logfuncs.__all__
+__all__ += loggers.__all__
+__all__ += utils.__all__
+
+
+CALLBACKS: dict[str, type[callbacks.Callback]] = {
+    "CallbackList"        : callbacks.CallbackList,
+    "CheckpointCallback"  : callbacks.CheckpointCallback,
+    "ConfigCallback"      : callbacks.ConfigCallback,
+    "EvaluationCallback"  : callbacks.EvaluationCallback,
+    "HParamCallback"      : callbacks.HParamCallback,
+    "KernelCallback"      : callbacks.KernelCallback,
+    "LRSchedulerCallback" : callbacks.LRSchedulerCallback,
+    "MetricsCallback"     : callbacks.MetricsCallback,
+    "ModelCallback"       : callbacks.ModelCallback,
+    "OptimizerCallback"   : callbacks.OptimizerCallback,
+    "ScalarsCallback"     : callbacks.ScalarsCallback,
+    "TableCallback"       : callbacks.TableCallback,
+    "WrapCallback"        : callbacks.WrapCallback,
 }  # fmt: skip
 r"""Dictionary of all available callbacks."""
 
 
-LOGFUNCS: dict[str, LogFunction] = {
-    "log_config"       : log_config,
-    "log_kernel"       : log_kernel,
-    "log_lr_scheduler" : log_lr_scheduler,
-    "log_metrics"      : log_metrics,
-    "log_model"        : log_model,
-    "log_optimizer"    : log_optimizer,
-    "log_plot"         : log_plot,
-    "log_table"        : log_table,
-    "log_values"       : log_values,
+LOGFUNCS: dict[str, logfuncs.LogFunction] = {
+    "log_config"       : logfuncs.log_config,
+    "log_kernel"       : logfuncs.log_kernel,
+    "log_lr_scheduler" : logfuncs.log_lr_scheduler,
+    "log_metrics"      : logfuncs.log_metrics,
+    "log_model"        : logfuncs.log_model,
+    "log_optimizer"    : logfuncs.log_optimizer,
+    "log_plot"         : logfuncs.log_plot,
+    "log_table"        : logfuncs.log_table,
+    "log_values"       : logfuncs.log_values,
 }  # fmt: skip
 r"""Dictionary of all available log functions."""
 
-LOGGERS: dict[str, type[Logger]] = {
-    "BaseLogger"    : BaseLogger,
-    "DefaultLogger" : DefaultLogger,
+LOGGERS: dict[str, type[loggers.Logger]] = {
+    "BaseLogger"    : loggers.BaseLogger,
+    "DefaultLogger" : loggers.DefaultLogger,
 }  # fmt: skip
 r"""Dictionary of all available loggers."""
