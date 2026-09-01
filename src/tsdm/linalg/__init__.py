9c6f370708061f279b19da11e4ee2c16ddefcbfe
Randolf Scholz <randolf.scholz@gmail.com>
2026-09-01 17:59:36 +0200

    refactor: restructure `linalg` submodules, simplify imports, and consolidate `__all__` definitions

--- src/tsdm/linalg/__init__.py
+++ src/tsdm/linalg/__init__.py
@@ -1,74 +1,23 @@
 r"""Linear Algebra Subroutines."""
 
 __all__ = [
-    # Logical Operators
-    "cumulative_and",
-    "cumulative_or",
-    "cumulative_xor",
-    # Matrix Functions
-    "closest_diagonal",
-    "closest_orthogonal",
-    "closest_skew",
-    "closest_symmetric",
-    "col_corr",
-    "erank",
-    "logarithmic_norm",
-    "matrix_norm",
-    "operator_norm",
-    "reldist",
-    "reldist_diagonal",
-    "reldist_orthogonal",
-    "reldist_skew",
-    "reldist_symmetric",
-    "relerank",
-    "row_corr",
-    "schatten_norm",
-    "spectral_abscissa",
-    "spectral_radius",
-    "stiffness_ratio",
-    # Tensor Functions
-    "geometric_mean",
-    "grad_norm",
-    "multi_norm",
-    "norm",
-    "scaled_norm",
-    "tensor_norm",
-    # rnn
-    "collate_packed",
-    "collate_padded",
-    "unpad_sequence",
-    "unpack_sequence",
+    # submodules
+    "collate",
+    "logical_operators",
+    "matrix_functions",
+    "tensor_functions",
+    "utils",
 ]
 
-from .collate import collate_packed, collate_padded, unpack_sequence, unpad_sequence
-from .logical_operators import cumulative_and, cumulative_or, cumulative_xor
-from .matrix_functions import (
-    closest_diagonal,
-    closest_orthogonal,
-    closest_skew,
-    closest_symmetric,
-    col_corr,
-    erank,
-    logarithmic_norm,
-    matrix_norm,
-    operator_norm,
-    reldist,
-    reldist_diagonal,
-    reldist_orthogonal,
-    reldist_skew,
-    reldist_symmetric,
-    relerank,
-    row_corr,
-    schatten_norm,
-    spectral_abscissa,
-    spectral_radius,
-    stiffness_ratio,
-)
-from .tensor_functions import (
-    geometric_mean,
-    grad_norm,
-    multi_norm,
-    norm,
-    scaled_norm,
-    tensor_norm,
-)
+from . import collate, logical_operators, matrix_functions, tensor_functions, utils
+from .collate import *  # ruff: ignore[F403]
+from .logical_operators import *  # ruff: ignore[F403]
+from .matrix_functions import *  # ruff: ignore[F403]
+from .tensor_functions import *  # ruff: ignore[F403]
+from .utils import *  # ruff: ignore[F403]
+
+__all__ += collate.__all__
+__all__ += logical_operators.__all__
+__all__ += matrix_functions.__all__
+__all__ += tensor_functions.__all__
+__all__ += utils.__all__
