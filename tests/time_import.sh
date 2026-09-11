7af93b0f7fae793359e51fb034031a3faf2bc62a
Randolf Scholz <randolf.scholz@gmail.com>
2026-09-11 09:16:53 +0200

    feat: add justfile for command management and remove old import timing scripts

--- tests/time_import.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# get project root with git
-cd "$(git rev-parse --show-toplevel)" || exit
-echo "Project root: $(pwd)"
-python -m cProfile -o tests/import_time.prof tests/x
-#python -X importtime -c "import tsdm" 2> tests/import_time.log
-snakeviz tests/import_time.prof
